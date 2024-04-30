684402d8b7a3620d34bd33b498fbf71d4390fdfd
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2024-04-30 23:15:24 +1000

    feat: add layout and header

---
+++ frontend/src/layouts/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
