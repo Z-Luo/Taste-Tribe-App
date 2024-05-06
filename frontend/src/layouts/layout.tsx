58712ef1abfaf62516b78231df7ce4cc8be8b121
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2024-05-06 17:09:49 +1000

    feat: user profile page

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -4,13 +4,14 @@ import Hero from "@/components/Hero";
 
 interface LayoutProps {
   children: React.ReactNode;
+  showHero?: boolean;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, showHero = false }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Hero />
+      {showHero && <Hero />}
       <div className="container mx-auto flex-1 py-10">{children}</div>
       <Footer />
     </div>
