6f914abddbfcb0cbadfb01d701432ee34ce20247
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2024-05-08 19:05:35 +1000

    fix: backend configure port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,8 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 
-app.listen(7000, () => {
-    console.log("server started on localhost:7000");
+const port = process.env.PORT || 7000;
+app.listen(port, () => {
+    console.log(`server started on port ${port}
+    `);
 })
