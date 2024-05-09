408598ca3527d9a13468d49402df30cd8014d42d
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2024-05-09 22:56:37 +1000

    feat: create restaurant api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import {v2 as cloudinary} from 'cloudinary';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import myUserRoute from './routes/MyUserRoute'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database"))
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 const app = express();
 
