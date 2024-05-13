d466b1c3af429bd23a7deceafd41bec3a8639a35
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2024-05-13 23:27:19 +1000

    feat: add detail form in manage restaurant

---
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,7 @@
+import ManageRestaurantForm from "@/form/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  return <ManageRestaurantForm />;
+};
+
+export default ManageRestaurantPage;
