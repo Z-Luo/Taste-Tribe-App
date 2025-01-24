ce3e5de3b9980117860095dcce7ee92b92d3d1ea
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2025-01-24 23:11:00 +1100

    add search navigate in homepage component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,15 @@
 import landingImg from "@/assets/landing.png";
 import appDownloadImg from "@/assets/appDownload.png";
-// import SearchBar from "@/components/SearchBar";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
       <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
@@ -10,7 +17,7 @@ const HomePage = () => {
           Tuck into a takeaway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
-        {/* <SearchBar placeHolder="Search by City" /> */}
+        <SearchBar placeHolder="Search by City" onSubmit={handleSearchSubmit} />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImg} />
