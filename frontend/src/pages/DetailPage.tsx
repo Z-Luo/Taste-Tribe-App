ff65a7989f640657cdb8260dc666b0192338327b
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2025-02-10 23:36:35 +1100

    frontend add user profile form on checkout

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -5,6 +5,7 @@ import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Card, CardFooter } from "@/components/ui/card";
+import { UserFormData } from "@/form/user-profile-form/UserProfileForm";
 import { IMenuItem } from "@/interfaces/restaurant";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -74,6 +75,10 @@ const DetailPage = () => {
     });
   };
 
+  const onCheckout = (userFormData: UserFormData) => {
+    console.log("userFormData", userFormData);
+  };
+
   if (isLoading || !restaurant) {
     return "Loading...";
   }
@@ -102,7 +107,10 @@ const DetailPage = () => {
               removeFromCart={removeFromCart}
             />
             <CardFooter>
-              <CheckoutButton />
+              <CheckoutButton
+                disabled={cartItems.length === 0}
+                onCheckout={onCheckout}
+              />
             </CardFooter>
           </Card>
         </div>
